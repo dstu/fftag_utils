cb822102f447ebe61cb3a87afa312084ced1dc67
Stu Black <trurl@freeshell.org>
2010-03-19 16:31:49 -0400

    Added some more options to fftag-query.pl.
    New options are:
    
    --substate: allow counting by substate numbers in addition to
      fftags and propbank labels
    
    --binarized: allow standard nonterminals plus those prefaced by
      "@", as in binarized trees
    
    --counts: print counts of nonterminal/fftag pairs instead of
      by-fftag proportions

--- fftag-query.pl
+++ fftag-query.pl
@@ -25,21 +25,43 @@ Output is printed to standard out.
 
 Options:
  -p,--propbank  Use propbank labels instead of form-function tags
+ -s,--substate  Use substate numbers instead of form-function tags
+ -b,--binarized Allow '\@'-style binarization
+ -c,--counts    Print counts instead of proportions
  -n,--nulls     Include "NONE" counts in proportions
 EOF
 
-my ($use_propbank, $count_nulls);
+my ($use_propbank, $use_substate, $allow_binarized, $count_nulls, $print_counts);
 
-GetOptions( "propbank" => \$use_propbank,
-            "nulls"    => \$count_nulls,
-            "help"     => sub { print $usage; exit 0 }, )
+GetOptions( "propbank"  => \$use_propbank,
+            "substate"  => \$use_substate,
+            "binarized" => \$allow_binarized,
+            "nulls"     => \$count_nulls,
+            "counts"    => \$print_counts,
+            "help"      => sub { print $usage; exit 0 }, )
     or die "$usage\n";
 
-my @fftags = $use_propbank ? propbank_labels : fftags;
+my @fftags;
+if ($use_propbank) {
+    @fftags = sort propbank_labels;
+} elsif ($use_substate) {
+    @fftags = (0..64);
+} else {
+    @fftags = sort fftags;
+}
 
-my %scores = map { $_ => { map { $_ => 0 } (@fftags, "NONE") } } nonterminals;
+my @nonterminals = nonterminals;
+
+if ($allow_binarized) {
+    @nonterminals = (@nonterminals, map { "\@$_" } @nonterminals);
+}
+
+@nonterminals = sort @nonterminals;
+
+my %scores = map { $_ => { map { $_ => 0 } (@fftags, "NONE") } } @nonterminals;
 
 my $reader = node_reader({ Tags => \@fftags,
+                           Nonterminals => \@nonterminals,
                            Separators => ['xx', '-'], });
 
 sub read_trees {
@@ -72,12 +94,34 @@ if (@ARGV) {
     read_trees(\*STDIN);
 }
 
-for my $nonterminal (sort(nonterminals())) {
-    my $total = sum(values(%{$scores{$nonterminal}}));
-    next unless $total;
-    for my $tag (sort @fftags) {
-        my $proportion = $total && $scores{$nonterminal}->{$tag} ? $scores{$nonterminal}->{$tag} / $total : 0.0;
-        print "$nonterminal\t$tag\t$proportion\n";
+print "TAG\t";
+if ($use_substate) {
+    print "SUBSTATE\t";
+} elsif ($use_propbank) {
+    print "PROPBANK\t";
+} else {
+    print "FFTAG\t";
+}
+if ($print_counts) {
+    print "COUNT\n";
+} else {
+    print "PROPORTION\n";
+}
+
+for my $nonterminal (@nonterminals) {
+    if ($print_counts) {
+        for my $tag (@fftags) {
+            next
+                unless $scores{$nonterminal}->{$tag};
+            print "$nonterminal\t$tag\t$scores{$nonterminal}->{$tag}\n";
+        }
+    } else {
+        my $total = sum(values(%{$scores{$nonterminal}}));
+        next unless $total;
+        for my $tag (@fftags) {
+            my $proportion = $total && $scores{$nonterminal}->{$tag} ? $scores{$nonterminal}->{$tag} / $total : 0.0;
+            print "$nonterminal\t$tag\t$proportion\n";
+        }
     }
 }
 
